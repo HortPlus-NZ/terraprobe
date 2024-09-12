e3af1d40dda02df5b891906d01062811d0af659a
Bailey Jewell <bailey@hortplus.com>
2024-09-12 19:18:31 +1200

    Update docker container to use uwsgi with sockets and self signed SSL cert locally

--- soil/soil/settings/dev_docker.py
+++ soil/soil/settings/dev_docker.py
@@ -6,6 +6,11 @@ SOIL_DB_HOST = get_env_variable('SOIL_DB_HOST')
 
 LOGLEVEL = "debug"
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
