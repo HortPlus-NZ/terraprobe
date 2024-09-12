7f5362212539807ca54e5da92883c50ee8fe5510
Bailey Jewell <bailey@hortplus.com>
2024-09-12 19:56:46 +1200

    Create additional config files for environments.

---
+++ soil/soil/settings/prod_docker.py
@@ -0,0 +1,22 @@
+from ._base import *
+
+SOIL_DB_HOST = get_env_variable('SOIL_DB_HOST')
+
+DEBUG = False
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': SOIL_DB_NAME,
+        'USER': SOIL_DB_USER,
+        'PASSWORD': SOIL_DB_PASSWORD,
+        'HOST': SOIL_DB_HOST,
+        'PORT': 5432,
+    }
+}
